59152bc5c496c6ce84859b604e2c1f56a5579282
何广鹏 <86814805+naixupan@users.noreply.github.com>
2025-05-27 15:47:59 +0800

    Update utils.h
    工具函数定义

--- utils.h
+++ utils.h
@@ -1,3 +1,28 @@
 #pragma once
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <math.h>
+
+using namespace std;
+
+// 加载相机内参
+/*
+输入：
+@camera_parameter_path：相机内参路径
+输出：
+@camera_patameter(Mat*)，指针
+*/
+
+void LoadCameraParameter(string camera_patameter_path);
+void LoadCalibrateMatrix(string matrix_path);
+void ReadRobotPoses(string poses_path);
+void EulerAngleToRotateMatrix();
+void RotateMatrixToEulerAngle();
+void R_T2HomogeneousMatrix();
+void HomogeneousMatrix2R_T();
+void Pose2HomogenousMatrix();
+void HomogeneousMatrix2Pose();
+void DetectChessBoard();
+void DetectArUcoMarks();
+void ComputeCameraPose();
+
