9c97a4b477ffe87a6b1f3ebdd0a60f407ac58bdd
何广鹏 <86814805+naixupan@users.noreply.github.com>
2025-06-05 08:28:28 +0800

    定位器设计

--- locator.h
+++ locator.h
@@ -2,3 +2,18 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
+class Locator
+{
+public:
+	Locator();
+	~Locator();
+};
+
+
+class Calibrator
+{
+public:
+	Calibrator();
+	~Calibrator();
+
+};
