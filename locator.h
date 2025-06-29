8698ccf5c5d211df9d659ca5e26d5256bd9dc241
何广鹏 <86814805+naixupan@users.noreply.github.com>
2025-06-29 09:36:22 +0800

    update

--- locator.h
+++ locator.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include "utils.h"
 
 class Locator
 {
@@ -15,5 +16,16 @@ class Calibrator
 public:
 	Calibrator();
 	~Calibrator();
+	void calibrate_eye_in_hand();
+	void calibrate_camera();
+	void calibrate_test();
+
+private:
+	std::string save_path;
+	std::string image_folder_path;
+	std::string robot_poses_path;
+
+	cv::Mat read_robot_poses();
+
 
 };
